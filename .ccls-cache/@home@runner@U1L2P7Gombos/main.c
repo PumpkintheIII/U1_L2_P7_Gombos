7b5ca0d7e9a4b396c9aec0832bcf9a8268e752ad
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-06 13:47:47 +0000

    Iteration 1

--- .ccls-cache/@home@runner@U1L2P7Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P7Gombos/main.c
@@ -6,6 +6,13 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int ageYears;
+  float ageDays;
+
+  printf("Enter how old you are in years: ");
+  scanf("%d", &ageYears);
+
+  ageDays = ageYears*365.25;
+  printf("Age in days: %.2f days", ageDays);
   return 0;
 }
